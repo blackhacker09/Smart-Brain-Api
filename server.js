e363a5812f3d3dc6d8511eb1ea798fd901f80aea
Suraj Verma <56511978+blackhacker09@users.noreply.github.com>
2023-05-23 00:07:48 +0530

    fixed code

--- server.js
+++ server.js
@@ -12,21 +12,21 @@ const db = knex({
     // connect to your own database here:
     client: 'pg',
     connection: {
-      // host: "127.0.0.1",
-      // port: 5432,
-      // user: "postgres",
-      // password: "test",
-      // database: "smart-brain",
+      host: "127.0.0.1",
+      port: 5432,
+      user: "postgres",
+      password: "test",
+      database: "smart-brain"
 
       // uncomment below for db connection
       
-      connectionString: process.env.DATABASE_URL,
-      ssl: { rejectUnauthorized: false },
-      host: process.env.DATABASE_HOST,
-      port: 5432,
-      user: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PW,
-      database: process.env.DATABASE_DB
+      // connectionString: process.env.DATABASE_URL,
+      // ssl: { rejectUnauthorized: false },
+      // host: process.env.DATABASE_HOST,
+      // port: 5432,
+      // user: process.env.DATABASE_USER,
+      // password: process.env.DATABASE_PW,
+      // database: process.env.DATABASE_DB
 
     }
 });
