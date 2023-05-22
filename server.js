8720f734a833e9a2345d2d23ebd662ab0572f090
Suraj Verma <56511978+blackhacker09@users.noreply.github.com>
2023-05-22 20:45:23 +0530

    update code

--- server.js
+++ server.js
@@ -36,8 +36,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res)=> { 'sucess connecting to backend' })
-// app.get('/', (req, res)=> { res.send(db.users) })
+
+app.get('/', (req, res)=> { res.send(db.users) })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
